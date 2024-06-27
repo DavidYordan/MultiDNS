e4eeba3ec98b915afa6d0f71038f83ab04d2dbcb
DavidYordan <xinburuquan@gmail.com>
2024-06-27 10:57:46 +0800

    update

--- internal/upstream.go
+++ internal/upstream.go
@@ -2,11 +2,11 @@ package internal
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/miekg/dns"
 )
@@ -19,9 +19,6 @@ func QueryUpstreamServer(s *DNSServer, dnsMsg *dns.Msg, isCN bool) ([]byte, stri
 		clients = httpClientsCN
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	resultChan := make(chan struct {
 		response []byte
 		server   string
@@ -31,7 +28,10 @@ func QueryUpstreamServer(s *DNSServer, dnsMsg *dns.Msg, isCN bool) ([]byte, stri
 
 	for _, upstreamAddr := range upstreamServers {
 		wg.Add(1)
-		go queryDoH(ctx, clients[upstreamAddr], dnsMsg, upstreamAddr, resultChan, &wg)
+		go func(upstreamAddr string, client *http.Client) {
+			defer wg.Done()
+			queryDoH(client, dnsMsg, upstreamAddr, resultChan)
+		}(upstreamAddr, clients[upstreamAddr])
 	}
 
 	go func() {
@@ -41,25 +41,18 @@ func QueryUpstreamServer(s *DNSServer, dnsMsg *dns.Msg, isCN bool) ([]byte, stri
 
 	for res := range resultChan {
 		if res.err == nil {
-			cancel()
-			if isCN {
-				return res.response, fmt.Sprintf("%s", res.server), nil
-			} else {
-				return res.response, fmt.Sprintf("%s_%d", res.server, s.SocksPort), nil
-			}
+			return res.response, res.server, nil
 		}
 	}
 
 	return nil, "", fmt.Errorf("failed to get a response from any upstream servers")
 }
 
-func queryDoH(ctx context.Context, client *http.Client, dnsMsg *dns.Msg, upstreamAddr string, resultChan chan struct {
+func queryDoH(client *http.Client, dnsMsg *dns.Msg, upstreamAddr string, resultChan chan struct {
 	response []byte
 	server   string
 	err      error
-}, wg *sync.WaitGroup) {
-	defer wg.Done()
-
+}) {
 	msg, err := dnsMsg.Pack()
 	if err != nil {
 		sendResult(upstreamAddr, nil, err, resultChan)
@@ -68,7 +61,7 @@ func queryDoH(ctx context.Context, client *http.Client, dnsMsg *dns.Msg, upstrea
 
 	dohURL := fmt.Sprintf("https://%s/dns-query", upstreamAddr)
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, dohURL, bytes.NewReader(msg))
+	req, err := http.NewRequest(http.MethodPost, dohURL, bytes.NewReader(msg))
 	if err != nil {
 		sendResult(upstreamAddr, nil, err, resultChan)
 		return
@@ -96,9 +89,12 @@ func sendResult(server string, response []byte, err error, resultChan chan struc
 	server   string
 	err      error
 }) {
-	resultChan <- struct {
+	select {
+	case resultChan <- struct {
 		response []byte
 		server   string
 		err      error
-	}{response: response, server: server, err: err}
+	}{response: response, server: server, err: err}:
+	case <-time.After(1 * time.Second):
+	}
 }
